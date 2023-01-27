ccd015f7804ff69537de6b17d831e1ff8d7d3a88
rk <rebeccakruk@outlook.com>
2023-01-27 16:28:33 -0700

    try block added for ValueError, done

--- app.py
+++ app.py
@@ -4,22 +4,21 @@ from calculators.multiplication import multiply
 from calculators.division import divide
 
 is_valid = False
-is_calcs = False
 
-
-calc_choice = int(input("Please select a calculation: 1. Add, 2. Subtract, 3. Multiply, 4. Divide "))
-
-# if calc_choice == int and calc_choice <1 and calc_choice >4:
-#         is_valid = True
-
-# elif calc_choice not in range(1-4):
-#         print("Out of range. Please select a number between 1 - 4")
-# elif calc_choice != int or calc_choice not in range(1,4):
-#         print("You entered something that isn't a number. Please select a number between 1 - 4")
-
-#         if (is_valid):
-num_one = int(input("Enter first number: "))
-num_two = int(input("Enter second number: "))
+while not is_valid:
+    calc_choice = input("Please select a calculation: 1. Add, 2. Subtract, 3. Multiply, 4. Divide ")
+    try:
+        calc_choice = int(calc_choice)
+        is_valid = True
+    except ValueError:
+        print("You entered something that isn't a number. Please select a number between 1 - 4")
+# I couldn't get the range to work :(
+    # except IndexError:
+    #     int(calc_choice <0 and calc_choice >5)
+    #     print("Out of range. Please enter a number between 1 - 4")
+    else:
+        num_one = int(input("Enter first number: "))
+        num_two = int(input("Enter second number: "))
 
 if calc_choice == 1:
     print("{} + {}".format(num_one,num_two))
@@ -37,9 +36,3 @@ elif calc_choice == 4:
     print("{} / {}".format(num_one,num_two))
     divide(num_one, num_two)
 
-
-            # is_calcs = True
-
-        # print("You entered something that isn't a number. Please select a number")
-
-        
